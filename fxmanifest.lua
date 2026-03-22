fde0bc5d3671e9e8b8456af3caeb889d80f9e5c3
QairDevelopment <qairdev@gmail.com>
2026-03-22 15:22:44 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+shared_script '@WaveShield/resource/include.lua'
+
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Qair'
+description 'Bomb Script Gun Repair Dependency'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
