a1b8fe2411ab6fb909f890b4115cc7d762ba9131
QairDevelopment <qairdev@gmail.com>
2026-03-22 15:22:00 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+
+Config.Center = vector3(3659.9558, -4769.1240, -2.6958)
+
+
+Config.Radius = 100.0
+
+Config.BombCount = 120
+
+
+Config.RefreshTime = 20000
+
+
+Config.ExplosionType = 1
+Config.ExplosionDamage = 60.0
+
+
+Config.TriggerRadius = 7.0
